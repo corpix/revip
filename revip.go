d6fc01796806a80bead83f632045c19b77ea2027
Dmitry Moskowski <me@corpix.ru>
2022-06-23 13:22:57 +0000

    add more methods around Container to control boxed configuration

--- revip.go
+++ revip.go
@@ -58,22 +58,48 @@ type Container struct {
 }
 
 // Unwrap returns a pointer to the inner configuration data structure.
-func (r *Container) Unwrap() interface{} { return r.config }
+func (r *Container) Unwrap() Config { return r.config }
+
+// EmptyClone returns empty configuration type clone.
+func (r *Container) EmptyClone() Config {
+	t := reflect.TypeOf(r.config)
+	return reflect.New(t).Interface()
+}
+
+// Empty allocates a new empty configuration, discarding any previously loaded data.
+func (r *Container) Empty() {
+	cfg := r.EmptyClone()
+	r.config = cfg
+}
+
+// Replace overrides internally stored configuration with passed value.
+func (r *Container) Replace(c Config) {
+	r.config = c
+}
 
 // Copy writes a shallow copy of the configuration into `v`.
-func (r *Container) Copy(v interface{}) error {
+func (r *Container) Copy(v Config) error {
 	return mapstructure.WeakDecode(r.config, v)
 }
 
+// Clone returns a shallow copy of the configuration with the same type.
+func (r *Container) Clone() (Config, error) {
+	v := r.EmptyClone()
+	err := r.Copy(v)
+	if err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
 // DeepCopy writes a deep copy of the configuration into `v`.
-func (r *Container) DeepCopy(v interface{}) error {
+func (r *Container) DeepCopy(v Config) error {
 	return mapstructure.Decode(r.config, v)
 }
 
-// Clone returns a deep copy of the configuration with the same type.
-func (r *Container) Clone() (Config, error) {
-	t := reflect.TypeOf(r.config)
-	v := reflect.New(t).Interface()
+// DeepClone returns a deep copy of the configuration with the same type.
+func (r *Container) DeepClone() (Config, error) {
+	v := r.EmptyClone()
 	err := r.DeepCopy(v)
 	if err != nil {
 		return nil, err
@@ -108,6 +134,21 @@ func (r *Container) Path(dst Config, path string) error {
 	}
 
 	return nil
+}
+
+// Default postprocess configuration with default values or returns an error.
+func (r *Container) Default() error {
+	return Postprocess(r.config, WithDefaults())
+}
+
+// Validate postprocess configuration with validation or returns an error.
+func (r *Container) Validate() error {
+	return Postprocess(r.config, WithValidation())
+}
+
+// Expand postprocess configuration with expansion or returns an error.
+func (r *Container) Expand() error {
+	return Postprocess(r.config, WithExpansion())
 }
 
 // New wraps configuration represented by `c` with come useful methods.
